4eb0b0d937a61742f3db155dc63c063d821de57b
Veer <81577902+veeeeeerrrrrr@users.noreply.github.com>
2026-02-24 16:09:11 +0530

    Update api_monitor.py

--- api_monitor.py
+++ api_monitor.py
@@ -1,9 +1,8 @@
 import aiohttp
 import asyncio
 import time
-from logger import log_info, log_error
-from config import API_LIST
 from db import insert_metric
+
 
 async def check_single_api(session, url):
     try:
@@ -12,20 +11,18 @@ async def check_single_api(session, url):
             latency = round((time.time() - start) * 1000, 2)
 
             if response.status == 200:
-                log_info(f"{url} UP | {latency} ms")
                 insert_metric(url, "UP", latency)
                 return (url, "UP", latency)
             else:
-                log_error(f"{url} ERROR {response.status}")
-                insert_metric(url, "ERROR", 0)
-                return (url, "ERROR", 0)
+                insert_metric(url, "ERROR", latency)
+                return (url, "ERROR", latency)
 
-    except Exception as e:
-        log_error(f"{url} DOWN | {str(e)}")
+    except Exception:
         insert_metric(url, "DOWN", 0)
         return (url, "DOWN", 0)
 
-async def check_apis_async():
+
+async def check_apis_async(api_list):
     async with aiohttp.ClientSession() as session:
-        tasks = [check_single_api(session, url) for url in API_LIST]
+        tasks = [check_single_api(session, url) for url in api_list]
         return await asyncio.gather(*tasks)
